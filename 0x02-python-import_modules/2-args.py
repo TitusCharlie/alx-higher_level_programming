220c1bac6c6ac207e6af8edb44aa56b2b2fc473d
TitusCharlie <tituslifecharlie@gmail.com>
2023-06-09 00:50:37 +0100

    2-args.py fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
+import argparse
 
 def list_of_argv():
-    string = input()
-    argv = string.split()
-    if len(argv) > 0:
-        if len(argv) > 1:
-            print(f"{len(argv)} arguments:")
-            num = 1
-            for value in argv:
+    string = argparse.ArgumentParser(description='print all the arguments in a particulat format')
+    string.add_argument('words', nargs='*')
+    word = string.parse_args()
+    result = word.words
+    if len(result) == 0:
+        print("0 argument")
+    if len(result) > 0:
+        if len(result) == 1:
+            print("1 argument:")
+        else:
+            print(f"{len(result)} arguments:")
+        num = 1
+        for value in result:
+            if len(result)== 1:
+                print(f"{num}: {value}")
+            else:
                 print(f"{num}: {value}")
                 num += 1
-        else:
-            print(f"1 argument:\n{len(argv)}: {string}")
-    else:
-        print("0 argument.")
 
 
 if __name__ == "__main__":
