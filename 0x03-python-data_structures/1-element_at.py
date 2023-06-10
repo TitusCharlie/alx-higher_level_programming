d7bcb2e79181b116f3e88b84197288d92b210d7e
TitusCharlie <tituslifecharlie@gmail.com>
2023-06-10 18:10:17 +0100

    task 1-element_at.py done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,12 +2,6 @@
 
 def element_at(my_list, idx):
     if idx >= 0 and idx < len(my_list):
-        return (my_list[idx])
+        return my_list[idx]
     else:
-        return (None)
-
-
-my_list = []
-idx = 0
-
-element_at(my_list, idx)
+        return None
