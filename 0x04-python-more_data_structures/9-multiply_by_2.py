82093485b2bc2ca658339fd493c60d4481965ab9
TitusCharlie <tituslifecharlie@gmail.com>
2023-06-20 07:29:07 +0100

    done

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     result = a_dictionary.copy()
-    for i in list(result):
-        result[i] *= 2
+    for i in list(a_dictionary):
+        result[i] *=2 
+#    for k, v in a_dictionary.items():
+ #       result[k] = v * 2
     return result
