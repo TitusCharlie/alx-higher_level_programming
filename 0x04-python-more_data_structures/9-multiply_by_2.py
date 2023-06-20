fd0ed0a34b440d045e28906da3b0b5f577b7cd5b
TitusCharlie <tituslifecharlie@gmail.com>
2023-06-20 07:41:09 +0100

    done

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     result = {}
-    for k, v in a_dictionary.items:
+    for k, v in a_dictionary.items():
         if type(v) is not int:
             result[k] = v
         else:
