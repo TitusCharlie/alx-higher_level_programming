01034a6a94a9f5939816aeda1b3846ed34887b98
TitusCharlie <tituslifecharlie@gmail.com>
2023-06-28 01:40:29 +0100

    done with 1,2,3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Define a class called square"""
+
 
 class Square():
+    """instantiate size to the class method"""
     def __init__(self, size=0):
+        """check for conditions true and false"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
+            """instantiate a private instance attribute"""
             self.__size = size
