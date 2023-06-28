773d05eb98ef2153f8ed09e8fe241ce98d268f57
TitusCharlie <tituslifecharlie@gmail.com>
2023-06-28 20:28:55 +0100

    all checked

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,14 +3,9 @@
 
 
 class Square:
-    #"""Represent a square."""
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
         self.size = size
 
     @property
