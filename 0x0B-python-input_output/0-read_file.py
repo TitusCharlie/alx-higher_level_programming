f4d3eb9432db38a738fc5fa001a636eb0423426e
TitusCharlie <tituslifecharlie@gmail.com>
2023-07-11 20:09:36 +0100

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """
-Function that reads a text file (UTF8) and prints it to stdout:
-
-    - Prototype: def read_file(filename="")
-    - You must use the with statement
-    - You don’t need to manage file permission or
-      file doesn't exist exceptions.
-    - You are not allowed to import any module
+==================
+module 0-read_file
+==================
 """
+
 
 def read_file(filename=""):
     with open(filename, "r", encoding="UTF-8") as f:
