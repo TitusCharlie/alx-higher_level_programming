02b015606a9b7dc5a0f2b359c5c99549bff9cf6c
TitusCharlie <tituslifecharlie@gmail.com>
2023-07-11 08:28:42 +0100

    done

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,7 @@ module with method is_same_class
 ================================
 """
 
+
 def is_same_class(obj, a_class):
     """check if obj is an instance of a_class"""
     if type(obj) is not a_class:
