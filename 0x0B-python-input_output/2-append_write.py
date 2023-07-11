72291b9ab42938671c73982d6165d2a6cdfd204a
TitusCharlie <tituslifecharlie@gmail.com>
2023-07-11 20:14:47 +0100

    done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+=====================
+module 2-append_write
+=====================
+"""
 
 
 def append_write(filename="", text=""):
+    """writes a string to a text file"""
+
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
