6c83b395ed38c608a14c724e5d90034ec9337c6f
TitusCharlie <tituslifecharlie@gmail.com>
2023-07-11 08:22:39 +0100

    done

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+===================================
+module with method is_kind_of_class
+===================================
+"""
+
+def is_kind_of_class(obj, a_class):
+    """check if obj is an instance of a class
+       if obj is an instance of an inherited class"""
+    return isinstance(obj, a_class)
