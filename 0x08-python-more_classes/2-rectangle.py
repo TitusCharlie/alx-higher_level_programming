ec61ec226a6d1f2e21768a1173e2e6d577922d55
TitusCharlie <tituslifecharlie@gmail.com>
2023-07-13 19:04:54 +0100

    done

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -9,41 +9,46 @@ class Rectangle:
         self.width = width
         self.height = height
 
-    """create a property method for private instance"""
     @property
     def width(self):
+        """return the width"""
+
         return self.__width
-    """retrieve the property instance"""
+
     @width.setter
     def width(self, value):
         """check if width is an integer"""
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    """create a property method for private instance"""
     @property
     def height(self):
+        """return height"""
+
         return self.__height
-    
-    """retrieve the property instance"""
+
     @height.setter
     def height(self, value):
         """check if height is an integer"""
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    """public instace method"""
     def area(self):
+        """return area"""
+
         return self.__width * self.__height
 
-    """public instance method"""
     def perimeter(self):
+        """return perimeter"""
+
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
