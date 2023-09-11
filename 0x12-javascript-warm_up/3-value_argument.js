c533dc72c58b35f526a7d77df08a753f1a88db47
TLC <97347331+TitusCharlie@users.noreply.github.com>
2023-09-11 23:57:28 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args[0]) {
-  console.log(args[0]);
+if (args[2]) {
+  console.log(args[2]);
 } else {
   console.log("No argument");
 }
