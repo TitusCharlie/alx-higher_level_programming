38907af4172e57f48057f5bf673b98ce932df307
tituscharlie <tituslifecharlie@gmail.com>
2023-09-12 17:00:36 +0100

    done

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+const arg1 = 'undefined';
+const arg2 = 'undefined';
 
-if (arg1 && arg2) {
-  console.log(`${arg1} is ${arg2}`);
+if (process.argv[2] && process.argv[3]) {
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
 } else {
-  const arg1 = 'undefined';
-  const arg2 = 'undefined';
   console.log(`${arg1} is ${arg2}`);
 }
