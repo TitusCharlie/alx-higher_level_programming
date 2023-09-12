f2403d76b5122939c5cc7e7e6297f4abb29fb145
tituscharlie <tituslifecharlie@gmail.com>
2023-09-12 17:11:10 +0100

    5-to_integer.js done

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,6 +6,6 @@ const intValue = parseInt(firstArg, 10);
 if (!isNaN(intValue)) {
   console.log(`My number: ${intValue}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
 
