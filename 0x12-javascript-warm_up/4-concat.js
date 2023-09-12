ebeb53dd86e35299ffee645cf599b2f64007328b
tituscharlie <tituslifecharlie@gmail.com>
2023-09-12 16:53:24 +0100

    done

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -6,7 +6,7 @@ const arg2 = process.argv[3];
 if (arg1 && arg2) {
   console.log(`${arg1} is ${arg2}`);
 } else {
-  let arg1 = 'undefined';
-  let arg2 = 'undefined';
+  const arg1 = 'undefined';
+  const arg2 = 'undefined';
   console.log(`${arg1} is ${arg2}`);
 }
