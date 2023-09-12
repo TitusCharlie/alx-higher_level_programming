b299b9bb05e1010563c6fca7a1b2a7057bcfc3a7
tituscharlie <tituslifecharlie@gmail.com>
2023-09-12 15:23:52 +0100

    done

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-if (args[2]) {
-  console.log(args[2]);
-} else {
-  console.log("No argument");
-}
+if (process.argv[2])
+	console.log(process.argv[2]);
+else
+	console.log("No argument");
