e38ed19fa08e6eceddb2e292b62239f480c2d7d2
tituscharlie <tituslifecharlie@gmail.com>
2023-09-13 21:33:23 +0100

    11-second_biggest.js done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,25 +1,29 @@
 #!/usr/bin/node
 
-//array of numbers
+// array of numbers
 
 let largestNum = parseInt(process.argv[2]);
 let secLargest = parseInt(process.argv[2]);
 
-if(process.argv.length < 4) {
-	console.log(0);
-	return;
+function secondLargest () {
+  if (process.argv.length < 4) {
+    console.log(0);
+    return;
+  }
+
+  // pick two numbers in the array
+  for (let i = 3; i < process.argv.length; i++) {
+    const currentNum = process.argv[i];
+    if (currentNum > secLargest) {
+      secLargest = currentNum;
+      if (currentNum > largestNum) {
+        secLargest = largestNum;
+        largestNum = currentNum;
+      }
+    }
+  }
+
+  console.log(secLargest);
 }
 
-//pick two numbers in the array
-for (let i = 3; i < process.argv.length; i++) {
-  let currentNum = process.argv[i];
-	if(currentNum > secLargest) {
-		secLargest = currentNum;
-		if(currentNum > largestNum) {
-			secLargest = largestNum;
-			largestNum = currentNum;
-		}
-	}
-}
-
-console.log(secLargest);
+secondLargest();
