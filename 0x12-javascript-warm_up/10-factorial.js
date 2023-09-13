6bccb1840ea1b9e5354b5e3fa31361aa8cb88243
tituscharlie <tituslifecharlie@gmail.com>
2023-09-13 14:36:20 +0100

    0x12-javascript-warm_up/10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 
-function factorial(number) {
+function factorial (number) {
   if (number === 0) {
     return 1;
   } else {
-      return factorial(number - 1) * number;
+    return factorial(number - 1) * number;
   }
 }
 
 const numberInt = parseInt(process.argv[2]);
 if (!isNaN(numberInt)) {
-  result = factorial(numberInt);
+  const result = factorial(numberInt);
   console.log(result);
 } else {
-    console.log('NaN');
+  console.log('NaN');
 }
