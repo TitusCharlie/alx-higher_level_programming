cb6f67d6529544a22c3fdac46a095f2c48ed6b87
tituscharlie <tituslifecharlie@gmail.com>
2023-09-13 21:24:39 +0100

    11-second_biggest.js done

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+//array of numbers
+
+let largestNum = process.argv[2];
+let secLargest = process.argv[2];
+
+if(process.argv.length < 4) {
+	console.log(0);
+	return;
+}
+
+//pick two numbers in the array
+for (let i = 3; i < process.argv.length; i++) {
+  let currentNum = process.argv[i];
+	if(currentNum > secLargest) {
+		secLargest = currentNum;
+		if(currentNum > largestNum) {
+			secLargest = largestNum;
+			largestNum = currentNum;
+		}
+	}
+}
+
+console.log(secLargest);
