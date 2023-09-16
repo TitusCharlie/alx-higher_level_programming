933692f98390ce3f14cd50809ea103a8bc7c01e8
tituscharlie <tituslifecharlie@gmail.com>
2023-09-16 17:58:01 +0100

    0-select_states.py done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,57 @@
 #!/usr/bin/python3
 
-
-"""
-    module that list all the states in the database
-"""
-
-import argparse
 import MySQLdb
+import sys
 
-if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(
-            description='List all states from a MySQL database.')
-    parser.add_argument('username', type=str, help='MySQL username')
-    parser.add_argument('password', type=str, help='MySQL password')
-    parser.add_argument('database', type=str, help='Database name')
-    args = parser.parse_args()
-
-    def list_states(username, password, database):
-
+def list_states(username, password, database):
+    try:
+        # Connect to the MySQL server
         connection = MySQLdb.connect(
                 user=username,
                 passwd=password,
                 db=database,
                 host='localhost',
-                port=3306)
+                port=3306
+                )
 
+        # Create a cursor object to execute SQL queries
         cursor = connection.cursor()
 
+        # Execute the SQL query to retrieve all states sorted by id
         cursor.execute('SELECT * FROM states ORDER BY id ASC')
 
-        result = cursor.fetchall()
+        # Fetch all the results
+        results = cursor.fetchall()
 
-        for ids in result:
-            print(ids)
+        # Print the results in the desired format
+        for row in results:
+            print(row)
 
-        cursor.close()
-        connection.close()
+    except MySQLdb.Error as e:
+        print(f"MySQL Error: {e}")
+    finally:
+        # Close the cursor and connection in a finally block to ensure cleanup
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
 
-    list_states(args.username, args.password, args.database)
+
+def main():
+    # Check for the correct number of command-line arguments
+    if len(sys.argv) != 4:
+        print('Usage: script.py <username> <password> <database>')
+        sys.exit(1)
+
+    # Parse command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Call the function to list states
+    list_states(username, password, database)
+
+
+if __name__ == '__main__':
+    main()
