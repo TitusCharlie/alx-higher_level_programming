a3be51ae3d5ca06ecadfe1432718a0166d68fe24
tituscharlie <tituslifecharlie@gmail.com>
2023-09-17 15:19:17 +0100

    2-my_filter_states.py done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -25,7 +25,7 @@ def filter_states(username, password, database, state_name):
         cursor = connection.cursor()
 
         query = """ SELECT * FROM states
-          WHERE name LIKE '{}'
+          WHERE name LIKE '%{}%'
           ORDER BY id ASC """.format(state_name)
 
         cursor.execute(query)
