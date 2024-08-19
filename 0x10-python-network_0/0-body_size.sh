0b5b0aff2ec227f50acb736c4b9acaf0fcf13eed
TLC <97347331+TitusCharlie@users.noreply.github.com>
2024-08-19 22:33:45 +0100

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-#Capture the URL from the first argument
-URL=$1
-
-#Send a GET request using curl, suppress output,and get the size of the response
-
-size=$(curl -sI "$URL" | grep -i Content-Length | awk '{print $2}')
-
-#Display the size of the response body
-echo "$size" 
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
