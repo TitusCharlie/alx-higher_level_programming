a820986ce46b1cead2718ab20fc58a373a053f0b
TLC <97347331+TitusCharlie@users.noreply.github.com>
2024-08-19 22:35:11 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-  echo "$0 <URL>"
-  exit 1
-
-fi
-
-URL=$1
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-if [ "$response" -ne 200 ]; then
-  echo "Error: Unexpected HTTP status code $response"
-  exit 1
-
-fi
-
-curl -s "$URL"
-echo "" 
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
